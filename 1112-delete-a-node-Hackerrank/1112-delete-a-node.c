9094f4499d43c699a06deafb37d9b02b58f7b368
jhansi008 <147475139+jhansi008@users.noreply.github.com>
2025-09-13 14:57:46 +0530

    Added solution - LeetHub

---
+++ 1112-delete-a-node-Hackerrank/1112-delete-a-node.c
@@ -0,0 +1,30 @@
+
+
+/*
+ * Complete the 'deleteNode' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+    SinglyLinkedListNode* temp = llist;
+    for(int i = 0 ; i < position ; i++){
+        temp = temp->next;
+    }
+    temp->next->data = temp->next->next->data;
+    return llist;
+}
+
